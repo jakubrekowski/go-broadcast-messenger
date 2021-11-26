11abc3febb38f0ff1ae6c2fca6e22a9111c563cc
Jakub Rekowski <jakub.rekowski@gmail.com>
2021-11-27 00:11:33 +0100

    connections changed to UDP - connections changed to UDP - added nicks
    `fixme`:
    - add detecting broadcast and setting them/defining manual

--- main.go
+++ main.go
@@ -6,10 +6,11 @@ import (
 	"io"
 	"net"
 	"os"
+	"strings"
 )
 
 func broadcastDial(message string) {
-	conn, err := net.Dial("tcp", "192.168.100.103:9000")
+	conn, err := net.Dial("udp", "192.168.100.255:9000")
 		if err != nil {
 			panic(err)
 		}
@@ -21,20 +22,46 @@ func broadcastDial(message string) {
 		conn.Close()
 }
 
-func keyboard() {
+func keyboard(nick string) {
 	for {
 		reader := bufio.NewReader(os.Stdin)
 		message, _ := reader.ReadString('\n')
-		broadcastDial(message)		
+
+		if message == "\n" {
+			continue
+		}
+
+		broadcastDial(fmt.Sprint("\033[33m", nick, "% \033[0m", message))
 	}
 }
 
 func main() {
-  fmt.Println("Broadcast Messenger v. alpha0.1")
+  fmt.Println("Broadcast Messenger v. alpha0.3")
 
-	go keyboard()
+// Nick selector
 
-	ln, err := net.Listen("tcp", ":9000")
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Print("Enter nick: ")
+	nick, _ := reader.ReadString('\n')
+
+	if nick == "\n" {
+		nick = "anonymous"
+	}
+
+	nick = strings.Replace(nick, "\n", "", -1)
+
+	broadcastDial(fmt.Sprint("\033[32m> ", nick, " has join the conversation.\n"))
+
+// main loop
+
+	go keyboard(nick)
+
+	addr := net.UDPAddr{
+		Port: 9000,
+		IP:  	net.ParseIP("192.168.100.255"),
+	}
+
+	ln, err := net.ListenUDP("udp", &addr)
 	if err != nil {
 		panic(err)
 	}
@@ -42,21 +69,21 @@ func main() {
 	defer ln.Close()
 
 	for {
-		conn, err := ln.Accept()
-		if err != nil {
-			panic(err)
-		}
+		// conn, err := ln.Accept()
+		// if err != nil {
+		// 	panic(err)
+		// }
 		
 		for {
 			bs := make([]byte, 1024)
-			_, err := conn.Read(bs)
+			_, err := ln.Read(bs)
 			if err != nil {
 				break
 			}
 
-			fmt.Print("[", conn.RemoteAddr(), "] ", string(bs[:]))
+			fmt.Print(string(bs[:]))
 		}
 
-		conn.Close()
+		ln.Close()
 	}
 }
